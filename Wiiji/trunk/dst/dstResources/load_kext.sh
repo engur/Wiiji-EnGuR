182cd6b259ba343a1a0d1cd76cc050a0cf47dc9d
veltropt <veltropt@da8e50c5-bb4b-0410-b3e0-84c6e952159c>
2008-05-02 05:28:42 +0000

    1.2 release

--- Wiiji/trunk/dst/dstResources/load_kext.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#killall -9 Wiiji
-#sleep 2
-# should I delete the kext too?
-if [ -d /System/Library/Extensions/virtualhid.kext ]
-then
-	/sbin/kextload /System/Library/Extensions/virtualhid.kext
-fi
